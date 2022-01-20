fa629bf335f63ac1904629a153a15ff9cca0737f
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2022-01-20 17:35:04 -0300

    feat: animacao ao scrill

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,6 +67,32 @@ function initScrollSuave() {
   });
 }
 
+// Animação ao Scroll
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowMetade = window.innerHeight * 0.2;
+
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
 initTabNav();
 initAccordion();
 initScrollSuave();
+initAnimacaoScroll();
